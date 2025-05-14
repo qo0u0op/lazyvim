7e1a073390edbf6069ee805e348bde35738360c6
Vikro Linne <vikrolinne07@outlook.com>
2025-05-14 15:34:05 +0800

    feat: support wsl

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+local in_wsl = vim.fn.has("wsl") == 1
+
+if in_wsl then
+  vim.g.clipboard = {
+    name = 'win32yank-wsl',
+    copy = {
+      ['+'] = 'win32yank.exe -i --crlf',
+      ['*'] = 'win32yank.exe -i --crlf',
+    },
+    paste = {
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = 0,
+  }
+end
