4dbaa4b4d9eef6336bb005a46c246efc9620baff
Vikro Linne <vikrolinne07@outlook.com>
2025-07-15 16:56:23 +0800

    refactor: extract config from plugins/config.lua and config/keymaps.lua

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -1,10 +1,10 @@
 return {
-  -- PlantUML
   {
     "javiorfo/nvim-soil",
     -- Optional for puml syntax highlighting:
     dependencies = { "javiorfo/nvim-nyctophilia" },
     ft = "plantuml",
+    keys = { { "<leader>cp", "<cmd>Soil<cr>", desc = "Soil plantuml" } },
     opts = {
       actions = {
         redraw = false,
