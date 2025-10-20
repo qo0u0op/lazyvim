c4321aa6f86cd328542a6e7f3e30b1c05be88cae
Vikro Linne <vikrolinne07@outlook.com>
2025-10-20 13:56:55 +0800

    revert(bufferline): remove temp fix

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -14,16 +14,6 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
-  -- fixme
-  {
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
   {
     "linux-cultist/venv-selector.nvim",
     branch = "main",
