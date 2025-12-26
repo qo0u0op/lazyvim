9e9623af2bb110ea087b5c75c46ad6192afe102c
Vikro Linne <vikrolinne07@outlook.com>
2025-12-27 04:09:06 +0800

    feat: change blink keymap

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,21 +3,55 @@ return {
     "saghen/blink.cmp",
     opts = {
       keymap = {
-        preset = "enter",
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<CR>"] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          "snippet_forward",
-          "fallback",
+        preset = "none",
+        ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+        -- ['<ESC>'] = { 'hide', 'fallback' },
+        ["<CR>"] = { "accept", "fallback" },
+
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<Left>"] = { "snippet_backward", "fallback" },
+        ["<Right>"] = { "snippet_forward", "fallback" },
+
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+      },
+      completion = {
+        keyword = {
+          -- 'prefix' will fuzzy match on the text before the cursor
+          -- 'full' will fuzzy match on the text before _and_ after the cursor
+          -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+          range = "full",
         },
-        ["<S-CR>"] = { "snippet_backward", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
+        list = {
+          selection = {
+            -- When `true`, will automatically select the first item in the completion list
+            preselect = false,
+            -- preselect = function(ctx) return vim.bo.filetype ~= 'markdown' end,
+
+            -- When `true`, inserts the completion item automatically when selecting it
+            -- You may want to bind a key to the `cancel` command (default <C-e>) when using this option,
+            -- which will both undo the selection and hide the completion menu
+            auto_insert = true,
+            -- auto_insert = function(ctx) return vim.bo.filetype ~= 'markdown' end
+          },
+        },
+      },
+      sources = {
+        -- `lsp`, `buffer`, `snippets`, `path` and `omni` are built-in
+        -- so you don't need to define them in `sources.providers`
+        default = { "lsp", "buffer", "snippets", "path" },
+        -- providers = {
+        --   lsp = { score_offset = 3 },
+        --   buffer = { score_offset = 2 },
+        --   snippets = { score_offset = 1 },
+        --   path = { score_offset = 0 },
+        -- },
       },
     },
   },
