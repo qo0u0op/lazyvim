eab8ea9bfffc35df214d5dbe936db12a0ffdf3c7
Vikro Linne <vikrolinne07@outlook.com>
2025-12-27 03:18:41 +0800

    feat(jdtls): set jdtls maven settings path

---
+++ lua/plugins/jdtls.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      settings = {
+        java = {
+          configuration = {
+            maven = {
+              userSettings = vim.fn.expand("~/.config/maven/settings.xml"),
+            },
+          },
+        },
+      },
+    },
+  },
+}
