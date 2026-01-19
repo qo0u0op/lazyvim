ff715361617339aa11eca7777c242281c342e78c
Vikro Linne <vikrolinne07@outlook.com>
2026-01-19 21:17:56 +0800

    feat: set java lsp maven to fit xdg base directory

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -5,7 +5,18 @@ return {
       settings = {
         java = {
           configuration = {
-            maven = { userSettings = vim.fn.expand("~/.config/maven/settings.xml") },
+            maven = {
+              userSettings = vim.fn.expand("~/.config/maven/settings.xml"),
+            },
+          },
+          jdt = {
+            ls = {
+              vmargs = table.concat({
+                "-Djava.util.prefs.userRoot=" .. vim.env.XDG_CONFIG_HOME .. "/java",
+                -- "-Xmx2g",
+                -- "-Xms512m",
+              }, " "),
+            },
           },
         },
       },
