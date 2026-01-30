5bc3756a9ff2bfbb4ff55e49e6c0640ac48adbe6
Vikro Linne <vikrolinne07@outlook.com>
2026-01-30 19:29:38 +0800

    feat: add dap and lang, go, r, rust

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -1,17 +1,21 @@
 return {
   -- import Lazyvim extras:
   { import = "lazyvim.plugins.extras.ai.sidekick" },
-  { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.coding.yanky" },
+  { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.editor.fzf" },
   { import = "lazyvim.plugins.extras.editor.mini-diff" },
   { import = "lazyvim.plugins.extras.util.dot" },
   -- import language:
   { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.java" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.r" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.sql" },
   { import = "lazyvim.plugins.extras.lang.toml" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
